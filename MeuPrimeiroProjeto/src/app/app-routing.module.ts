c33444d32c036d4b275348c6c8728385ba9002c3
pedroitalocampos <pedroitalocampos@gmail.com>
2022-07-15 12:21:33 -0300

    rotas

--- MeuPrimeiroProjeto/src/app/app-routing.module.ts
+++ MeuPrimeiroProjeto/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { FirstComponentComponent } from './componets/first-component/first-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:FirstComponentComponent},
+  {path:'list',component: ListRenderComponent},
+  {path:'sda',component: IfRenderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- MeuPrimeiroProjeto/src/app/app.module.ts
+++ MeuPrimeiroProjeto/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './componets/first-component/first-component.component';
 import { ParentDataComponent } from './componets/parent-data/parent-data.component';
